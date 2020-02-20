4c444299a00a5cd917415579df29c689e78f49c6
Reginaldo ferreira de sousa <reginaldodsferreira@gmail.com>
2020-02-20 10:33:58 -0300

    criada estrutura basica do projeto; criado o controller, views, model de planos 'plans'; intalação do sequelize-cli onde foi criado o primeiro model 'Plans' e feito primeira migração para o banco MySQL..

---
+++ services/PlansService.js
@@ -0,0 +1,85 @@
+//var Plan = require("../models/plan");
+var Database = require("../models/index");
+
+class PlansService {
+
+  constructor(){
+    this.Plan = Database["Plan"];
+  }
+
+  async listAll(){
+    let dbPlan = {};
+    var errors = {};
+    try {
+      dbPlan = await this.Plan.findAll({ order:[['id','DESC']], limit: 4 });
+      return dbPlan;
+      //  console.log(dbPlan);
+    } catch (e) {
+
+      errors.system_msg = "Não foi possível Carregar os planos";
+
+      return errors;
+    }
+
+  }
+
+
+  async store(plans){
+    var errors = {};
+    if(plans.import != undefined){
+      plans.import = true;
+    }else {
+      plans.import = false;
+    }
+
+    var isValid = this.validate(plans, errors);
+
+    if (isValid) {
+      try {
+        await this.Plan.create(plans);
+        return true;
+      } catch (e) {
+
+        errors.system_msg = "Não foi possível salvar o plano";
+        return errors;
+      }
+    }else {
+      return errors;
+    }
+    // Plan.create(plans).then(() => {
+    //
+    // }).catch(err => {
+    //
+    // });
+  }
+
+  validate(plan, errors){
+    var erroCount = 0;
+    if(plan.title == undefined){
+      errors.title_msg = "o título é inválido";
+      erroCount++;
+    }else {
+      if(plan.title.length < 3){
+        errors.title_msg = "o título muito pequeno"
+        erroCount++;
+      }
+    }
+
+    if(plan.list  == undefined){
+      errors.list_msg = "A quantidade de listas é inválida"
+      erroCount++;
+    }else {
+      if(plan.list < 1){
+        errors.list_msg = "A quantidade de listas é inválida"
+        erroCount++;
+      }
+    }
+    if (erroCount == 0) {
+      return true
+    }else {
+      return false;
+    }
+  }
+
+}
+module.exports = new PlansService();
