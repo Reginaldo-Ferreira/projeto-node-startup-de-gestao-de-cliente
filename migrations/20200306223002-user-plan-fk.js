0f0ec0860718c27b6663b7d10b82cd1d050d6242
Reginaldo ferreira de sousa <reginaldodsferreira@gmail.com>
2020-03-06 19:51:13 -0300

    excução do código de migração. adicionado a chave estrangeira 'fk' na tabela users com nome 'plans_id'

--- migrations/20200306223002-user-plan-fk.js
+++ migrations/20200306223002-user-plan-fk.js
@@ -2,13 +2,16 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.addColumn("users",'plan_id',{ // adiciona col na tabela users com nome 'plan_id'
+      type: Sequelize.DataTypes.INTEGER, // tipo da coluna
+      references:{ 
+        model: 'Plans', // associando ou criando relacionamento da coluna com a tabela plans
+        key: 'id' // assiciando com a coluna 'id' da tabela 'plans'
+      },
+      //PROTEÇÕES 
+      onDelete: 'SET NULL', // SE O PLANO FOR DELETA DO SETAR 'null' na coluna 'plan_id' da tabela 'users'
+      onUpdate: 'CASCADE'  // SE O NUMERO DO PLANO FOR ALTERADO MUDA O NUMERO ONDE ESTIVER SENDO ULTILIZADO
+    })
   },
 
   down: (queryInterface, Sequelize) => {
